51d924f04526ae083b00bd4e0040c44f98f526a6
tasfiaSeuti <31209824+tasfiaSeuti@users.noreply.github.com>
2022-08-28 00:29:35 +0600

    Add files via upload

---
+++ Memory-Leak-Detection-main/app/src/main/java/com/lukaslechner/memoryleakexample/LeakingActivity.kt
@@ -0,0 +1,30 @@
+package com.lukaslechner.memoryleakexample
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class LeakingActivity : AppCompatActivity() {
+
+    private val listener = Listener()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leaking)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        GlobalSingleton.register(listener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        // GlobalSingleton.unregister(listener)
+    }
+
+    // inner class has implicit reference to enclosing Activity
+    private inner class Listener : GlobalSingletonListener {
+        override fun onEvent() {}
+    }
+}
